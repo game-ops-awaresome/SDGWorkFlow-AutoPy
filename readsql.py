aae959f1fc13bc7da1e2e360700ca2c1b51dacd0
e <584747152@qq.com>
2018-10-08 16:40:02 +0800

    Update ——数据库读取优化

--- readsql.py
+++ readsql.py
@@ -2,24 +2,26 @@
 import pymysql
 
 class SQLOperate():
-    def GetLoginname(self, name):
-        conn = pymysql.connect(
+    conn = pymysql.connect(
             host= '10.246.190.99',
             port = 3306,
             user = 'outsourcing',
             password = '99Outsourcing!@#',
             database = "outsourcing",
             ) 
-        cursor = conn.cursor()
+    def GetLoginname(self, name):#拉取
+        cursor = self.conn.cursor()
         # 使用 execute()  方法执行 SQL 查询
         cursor.execute("SELECT loginname from `user` WHERE username = '"+name+"'")
  
         # 使用 fetchone() 方法获取单条数据.
         data = "".join(cursor.fetchone())
         # 关闭数据库连接
-        conn.close()
+        self.conn.close()
         return data
-        
+
+
 if __name__ == "__main__":
     mission = SQLOperate()
     print(mission.GetLoginname( '闵银花' ))
+
